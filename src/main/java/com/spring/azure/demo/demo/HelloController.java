c79a8462b52536b2be118fee0dfd002b9fb17e61
Saravanan U <mailmebysaro@gmail.com>
2024-10-07 22:45:23 +0530

    added jpa

--- src/main/java/com/spring/azure/demo/demo/HelloController.java
+++ src/main/java/com/spring/azure/demo/demo/HelloController.java
@@ -1,12 +1,31 @@
 package com.spring.azure.demo.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
+    @Autowired
+    private UserRepository userRepository;
     @GetMapping("ping")
     public String sayHello() {
         return "Hello Saro, Welcome to Azure!!!";
+    }
+
+    @GetMapping("users")
+    public List<AzureUser> users() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping(value = "users", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public AzureUser user(@RequestBody AzureUser azureUser) {
+        userRepository.save(azureUser);
+        return azureUser;
     }
 }
